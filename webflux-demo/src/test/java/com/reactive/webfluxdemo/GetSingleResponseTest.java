dd83d20dfe1db4ebefd3678cb84946136ce3d175
Kanav Ghai <kanghai@publicisgroupe.net>
2023-10-23 13:30:43 +0530

    chore: test the webClient using StepVerfier

--- webflux-demo/src/test/java/com/reactive/webfluxdemo/GetSingleResponseTest.java
+++ webflux-demo/src/test/java/com/reactive/webfluxdemo/GetSingleResponseTest.java
@@ -4,11 +4,13 @@ import com.reactive.webfluxdemo.dto.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 public class GetSingleResponseTest extends BaseTest {
 
     @Autowired
-     private WebClient webClient;
+    private WebClient webClient;
 
     @Test
     public void blockTest() {
@@ -20,6 +22,21 @@ public class GetSingleResponseTest extends BaseTest {
                 .block(); // Do not use
 
         System.out.println(response);
+
+    }
+
+    @Test
+    public void stepVerifierTest() {
+        Mono<Response> responseMono = this.webClient
+                .get()
+                .uri("reactive-math/square/{number}", 5)
+                .retrieve()
+                .bodyToMono(Response.class);
+
+
+        StepVerifier.create(responseMono)
+                .expectNextMatches(r -> r.getOutput() == 25)
+                .verifyComplete();
 
     }
 }
